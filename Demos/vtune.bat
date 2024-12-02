f36e7844977902a9019de137932e700a977745aa
Laurent Meyer <lmeyer@ea4d.com>
2024-12-02 20:16:55 +0100

    Demo05

--- Demos/vtune.bat
+++ Demos/vtune.bat
@@ -3,5 +3,6 @@ map2pdb.exe -bind:Demo01.exe Demo01.map
 map2pdb.exe -bind:Demo02.exe Demo02.map
 map2pdb.exe -bind:Demo03.exe Demo03.map
 map2pdb.exe -bind:Demo04.exe Demo04.map
+map2pdb.exe -bind:Demo05.exe Demo05.map
 pause
  
