84214d29555dc85f6ce7c83640a667180dd295e4
Laurent Meyer <lmeyer@ea4d.com>
2024-12-26 15:48:55 +0100

    Miscellaneous

--- Demos/Demo03/uDemo03.pas
+++ Demos/Demo03/uDemo03.pas
@@ -26,24 +26,24 @@ type
     { Public declarations }
   end;
 
-  TPrimitives = class(TJX3Object)
+  TPrimitive = class(TJX3Object)
     Str: TJX3Str;
     Bool: TJX3Bool;
-    b: TJX3Num; // as Int
-    c: TJX3Num; // as UInt
-    d: TJX3Num; // as Int64
-    e: TJX3Num; // as UInt64
+    I: TJX3Num; // as Int
+    UI: TJX3Num; // as UInt
+    Dble: TJX3Num; // as Double
+    Curr: TJX3Num; // as Vurrency
     NullStr: TJX3Str;
     // ...
   end;
 
   TObjectDemo = class(TJX3Object)
-    S: TJX3Str;
-    X: TJX3List<TJX3Str>;                         // an array(List) of strings : TArray<string>
-    Y: TJX3Dic<TJX3Num>;                          // An dictionary of Numbers (<string, number>)  *JSON allows only strings as key
-    Z: TJX3List<TPrimitives>;                     // A list of TPrimitives
-    Q: TJX3List<TJX3List<TJX3Str>>;               // A list of string Lists
-    W: TJX3List<TJX3Dic<TJX3List<TPrimitives>>>;  // ouch ! A List of dictionaries of TPrimitives Objects Lists !!!
+    Str:  TJX3Str;
+    Keys: TJX3List<TJX3Str>;                            // an array(List) of strings : TArray<string>
+    Nums: TJX3Dic<TJX3Num>;                             // An dictionary of Numbers (<string, number>)  *JSON allows only strings as key
+    Primitives: TJX3List<TPrimitive>;                   // A list of TPrimitives
+    SLists: TJX3List<TJX3List<TJX3Str>>;                // A list of string Lists
+    PDicList: TJX3List<TJX3Dic<TJX3List<TPrimitive>>>;  // ouch ! A List of dictionaries of TPrimitives Objects Lists !!!
   end;
 
 var
@@ -62,68 +62,67 @@ begin
   Memo1.Lines.Clear;
 
   Demo := TObjectDemo.Create;
-  Demo.S.V := '~~😃~~'; // UTF8 Support
+  Demo.Str.V := '~~😃~~'; // UTF8 Support
 
   // TJX3List<TJX3Str> : Array<string>
   Memo1.lines.add('TJX3List<TJX3Str> : Array<string> :');
-  Demo.X.Add(TJX3Str.New('@@@@'));  // "C" to Create with a Value
-  Demo.X.Add(TJX3Str.New('EEZZ'));
-  Demo.X.Add(TJX3Str.New('OOOO'));
+  Demo.Keys.Add(TJX3Str.New('Q W E R T Y'));
+  Demo.Keys.Add(TJX3Str.New('A Z E R T Y'));
   Json := TJX3Object.ToJson(Demo, [joNullToEmpty]);
   Memo1.lines.add(Json);
 
-  //  + TJX3Dic<TJX3Num> : Dictionary<string, number> (JSON only allows strings as keys)
+  // + TJX3Dic<TJX3Num> : Dictionary<string, number> (JSON only allows strings as keys)
   Memo1.lines.add('');
   Memo1.lines.add('TJX3Dic<TJX3Num> : Dictionary<string, number> :');
-  Demo.Y.Add('Value1', TJX3Num.NewInt(1111));
-  Demo.Y.Add('Value2', TJX3Num.NewInt64(2222));
-  Demo.Y.Add('Value3', TJX3Num.NewInt64(3333));
-  Demo.Y.Add('Value4', TJX3Num.NewInt64(4444));
+  Demo.Nums.Add('Int', TJX3Num.NewInt(1111));
+  Demo.Nums.Add('Int64', TJX3Num.NewInt64(2222));
+  Demo.Nums.Add('Double', TJX3Num.NewDouble(33.33));
+  Demo.Nums.Add('Currency', TJX3Num.NewCurrency(44.44));
   Json := TJX3Object.ToJson(Demo, [joNullToEmpty]);
   Memo1.lines.add(Json);
 
-   //  + TJX3List<TPrimitives>  : Array<TPrimitives)
+  // TJX3List<TPrimitives> : Array<TPrimitives)
   Memo1.lines.add('');
   Memo1.lines.add('TJX3List<TPrimitives>  : Array<TPrimitives) :');
-  Demo.Z.Add(TPrimitives.Create);
-  Demo.Z.Last.Bool.V := True;
-  Demo.Z.Last.e.Int64 := 111;
-  Demo.Z.Add(TPrimitives.Create);
-  Demo.Z.Last.Bool.V := False;
-  Demo.Z.Last.e.Int64 := 333;
+  Demo.Primitives.Add(TPrimitive.Create);
+  Demo.Primitives.Last.Bool.Value := True;
+  Demo.Primitives.Last.I.Int64 := 111;
+  Demo.Primitives.Add(TPrimitive.Create);
+  Demo.Primitives.Last.Bool.Value := False;
+  Demo.Primitives.Last.Dble.Int64 := 333;
   Json := TJX3Object.ToJson(Demo, [joNullToEmpty]);
   Memo1.lines.add(Json);
 
-  //  + TJX3List<TJX3List<TJX3Str>> : Array<Array<string>>>
+  // TJX3List<TJX3List<TJX3Str>> : Array<Array<string>>>
   Memo1.lines.add('');
   Memo1.lines.add('TJX3List<TJX3List<TJX3Str>> : Array<Array<string>>> :');
   S := TJX3List<TJX3Str>.Create;
   S.Add(TJX3Str.New('TTT'));
   S.Add(TJX3Str.New('OOO'));
-  Demo.Q.Add(S);
+  Demo.SLists.Add(S);
   S := TJX3List<TJX3Str>.New;
-  S.AddRange([TJX3Str.New('UUU'), TJX3Str.New('III')]);
-  Demo.Q.Add(S);
+  S.AddRange([TJX3Str.New('XXX'), TJX3Str.New('YYY'), TJX3Str.New('ZZZ')]);
+  Demo.SLists.Add(S);
   Json := TJX3Object.ToJson(Demo, [joNullToEmpty]);
   Memo1.lines.add(Json);
   Memo1.lines.add('');
   Json := TJX3Object.ToJson(Demo, [joNullToEmpty]);
   Memo1.lines.add(Json);
 
-  // +  TJX3List<TJX3Dic<TJX3List<TPrimitives>>>  : Array<Dictionary<string, Array<TPrimitives>>> :)
+  // TJX3List<TJX3Dic<TJX3List<TPrimitives>>> : Array<Dictionary<string, Array<TPrimitives>>> :)
   Memo1.lines.add('');
   Memo1.lines.add('TJX3List<TJX3Dic<TJX3List<TPrimitives>>>  : Array<Dictionary<string, Array<TPrimitives>>>');
-  var p1 := TJX3List<TPrimitives>.NewAdd(TPrimitives.Create);              // Create a 2 elements Primitives array
+  var p1 := TJX3List<TPrimitive>.NewAdd(TPrimitive.Create);              // Create a 2 elements Primitives array
   p1.First.Str.v := 'Boolean1';                                            // Acdess the Last item (which is the first also in this case)
   p1.First.Bool.v := True;
-  var p2 := TJX3List<TPrimitives>.NewAdd(TPrimitives.Create);              // Create a 2 elements Primitives array
-  p2[0].Str.V := 'Boolean3';
+  var p2 := TJX3List<TPrimitive>.NewAdd(TPrimitive.Create);              // Create a 2 elements Primitives array
+  p2[0].Str.V := 'Boolean2';
   p2[0].Bool.v := True;
-  var d1 := TJX3Dic<TJX3List<TPrimitives>>.Create;                        // Create the dictionary ownning the 2 lists
+  var d1 := TJX3Dic<TJX3List<TPrimitive>>.Create;                        // Create the dictionary ownning the 2 lists
   d1.Add('DicVal1', p1);
   d1.Add('DicVal2', p2);
-  Demo.w.Add(d1);                                                         // Adding the Dict and its clone to the main list
-  Demo.w.Add( TJX3.Clone< TJX3Dic<TJX3List<TPrimitives>> >(d1));
+  Demo.PDicList.Add(d1);                                                         // Adding the Dict and its clone to the main list
+  Demo.PDicList.Add( TJX3.Clone< TJX3Dic<TJX3List<TPrimitive>> >(d1));
   Json := TJX3Object.ToJson(Demo, [joNullToEmpty]);
   Memo1.lines.add(Json);
 
